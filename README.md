92003a2baec655094e99c90d9ae720dba50b89e9
rakibul-ce-bd <53364534+rakibul-ce-bd@users.noreply.github.com>
2020-08-28 11:42:24 +0600

    Initial commit
    Added: README.md & script.sh

---
+++ README.md
@@ -0,0 +1,22 @@
+# Automatic Updater for OpenUserBot Remix
+
+
+![logo](https://telegra.ph/file/c5e2869d44546ef829281.jpg)
+
+
+
+### Steps:
+
+##### 1. Clone the repository (if exists, then ignore)
+
+```bash
+git clone https://github.com/rakibul-ce-bd/oub-remix-updater
+```
+
+##### 2. Run the below command to run the script:
+
+```bash
+. oub-remix-updater/s*
+```
+
+The script will do the process automatically.
---
+++ script.sh
@@ -0,0 +1,43 @@
+#Update script of oub-remix by @marshmello61
+#Modified by @rakibul-ce-bd
+
+
+repo="oub-remix"
+rm -rf ${repo}
+
+echo "You're running the OpenUserBot Remix Updater script"
+echo " "
+# Get username of user
+echo -n "Enter your GitHub username: "
+read username
+echo " "
+
+# Check if user has forked or has the same repo name
+echo -n "Have you forked oub-remix from @sahyam2019 or has the same repo name i.e. oub-remix? [y/n]: "
+read fork
+echo " "
+
+if [[ "${fork}" == 'y' ]]; then
+	git clone https://github.com/${userName}/${repo}
+	cd ${repo}
+elif [[ "${fork}" == 'n' ]]; then
+	echo -n "Enter your repo name: "
+	read repo
+	git clone https://github.com/${userName}/${repo}
+	cd ${repo}
+else
+        echo " "
+        echo "You were only supposed to enter y or n"
+        exit 1
+fi
+
+echo " "
+echo "Updating your oub-remix"
+git pull https://github.com/sahyam2019/oub-remix
+git diff
+git commit -m "Update: New commit(s)"
+git push
+echo " "
+echo "Updated"
+cd ..
+rm -rf ${repo}
